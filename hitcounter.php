89b4fd9e8eda6f818c70df1b0133313d8ea1e6ce
tomdebruin <tomdebruin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-03 18:12:02 +0000

    Slight code tidy
    git-svn-id: https://plugins.svn.wordpress.org/hitcounter/trunk@328331 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hitcounter.php
+++ hitcounter.php
@@ -59,39 +59,39 @@ $hc_options = get_option('hitcounter_options');
 /**
  * Create the database
  */
-	function hc_install() {
-		global $wpdb, $hc_db_version, $hc_table;
-	
-		// create event table
-		$sql = "CREATE TABLE " . $hc_table . " (
-			post_id mediumint(9) NOT NULL,
-			bot_count mediumint(9) DEFAULT '0' NOT NULL,
-			reg_count mediumint(9) DEFAULT '0' NOT NULL,
-			unreg_count mediumint(9) DEFAULT '0' NOT NULL,
-			UNIQUE KEY post_id (post_id)
-		);";
-	
-		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
-	
-		// create table if it doesn't exist
-		if ($wpdb->get_var("show tables like '$hc_table'") != $hc_table) {
-	
-			dbDelta($sql);
-			add_option( 'hc_db_version', $hc_db_version, '', 'yes' );
-			
-		}
-	
-		// update table if things have changed.
-		$installed_ver = get_option( "hc_db_version" );
+function hc_install() {
+	global $wpdb, $hc_db_version, $hc_table;
+
+	// create event table
+	$sql = "CREATE TABLE " . $hc_table . " (
+		post_id mediumint(9) NOT NULL,
+		bot_count mediumint(9) DEFAULT '0' NOT NULL,
+		reg_count mediumint(9) DEFAULT '0' NOT NULL,
+		unreg_count mediumint(9) DEFAULT '0' NOT NULL,
+		UNIQUE KEY post_id (post_id)
+	);";
+
+	require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+
+	// create table if it doesn't exist
+	if ($wpdb->get_var("show tables like '$hc_table'") != $hc_table) {
+
+		dbDelta($sql);
+		add_option( 'hc_db_version', $hc_db_version, '', 'yes' );
 		
-		if ( $installed_ver != $hc_db_version ) {
-	
-			dbDelta($sql);
-			update_option( 'hc_db_version', $hc_db_version, '', 'yes' );
-		}
 	}
+
+	// update table if things have changed.
+	$installed_ver = get_option( "hc_db_version" );
 	
-	register_activation_hook(__FILE__, 'hc_install');
+	if ( $installed_ver != $hc_db_version ) {
+
+		dbDelta($sql);
+		update_option( 'hc_db_version', $hc_db_version, '', 'yes' );
+	}
+}
+
+register_activation_hook(__FILE__, 'hc_install');
 
 /**
  * detect user agent to determine if bot, user, or logged in user
@@ -101,43 +101,43 @@ $hc_options = get_option('hitcounter_options');
  * @param $content - pass in the content of the post, perform the action.
  * @return - return post content after having incremented view count
  */
-	function detectAgent($content) {
-		global $hc_options, $current_user;
+function detectAgent($content) {
+	global $hc_options, $current_user;
 
-		if ( is_single() ) {
-	
-			$post_id = get_the_ID();
-			$agent = strtolower($_SERVER['HTTP_USER_AGENT']);
+	if ( is_single() ) {
 
-			if ( is_user_logged_in() ) {
-			
-				// if user is logged in get the user info
-				// and check if we want to log the user
-				get_currentuserinfo();
-				$capability = $hc_options['rolelevel'];
-				$ignore_author = $hc_options['author'];
+		$post_id = get_the_ID();
+		$agent = strtolower($_SERVER['HTTP_USER_AGENT']);
 
-				// if ignore author is set and current user is the author do nothing.
-				if ( $ignore_author == '1' && $current_user->ID == get_the_author_meta(ID) ) {
-					// do nothing
-				} else {	
-					// check capability of current user - compare to ignore option.
-					// If no users are ignored the $capability is 0.
-					if ( !current_user_can( $capability ) || $capability == '0' ) {
-							$count = 'reg';
-					}
+		if ( is_user_logged_in() ) {
+		
+			// if user is logged in get the user info
+			// and check if we want to log the user
+			get_currentuserinfo();
+			$capability = $hc_options['rolelevel'];
+			$ignore_author = $hc_options['author'];
+
+			// if ignore author is set and current user is the author do nothing.
+			if ( $ignore_author == '1' && $current_user->ID == get_the_author_meta(ID) ) {
+				// do nothing
+			} else {	
+				// check capability of current user - compare to ignore option.
+				// If no users are ignored the $capability is 0.
+				if ( !current_user_can( $capability ) || $capability == '0' ) {
+						$count = 'reg';
 				}
-
-			} elseif ( preg_match('(mozilla|gecko|khtml|msie|presto|trident|opera|blackberry|htc|\blg|mot|nokia|playstation|psp|samsung|sonyericsson)', $agent) ) {
-				$count = 'unreg';
-			} else {
-				$count = 'robot';
 			}
-			
-			logCount($count, $post_id);
+
+		} elseif ( preg_match('(mozilla|gecko|khtml|msie|presto|trident|opera|blackberry|htc|\blg|mot|nokia|playstation|psp|samsung|sonyericsson)', $agent) ) {
+			$count = 'unreg';
+		} else {
+			$count = 'robot';
 		}
-		return $content;
+		
+		logCount($count, $post_id);
 	}
+	return $content;
+}
 /*	
 if ( $hc_options['excerpt'] )
 	add_filter('the_excerpt', 'detectAgent');
@@ -153,23 +153,23 @@ else
  * @param $post_id - ID if the currently viewed post.
  * @return - nothing.
  */
-	function logCount($count, $post_id) {
-		global $wpdb, $hc_table;
-		
-		switch ($count) {
-    	case 'reg':
-        	$wpdb->query("INSERT INTO ".$hc_table." (post_id, reg_count) VALUES (".$post_id.", 1) ON DUPLICATE KEY UPDATE reg_count = reg_count+1");
-        	break;
-   		case 'unreg':
-			$wpdb->query("INSERT INTO ".$hc_table." (post_id, unreg_count) VALUES (".$post_id.", 1) ON DUPLICATE KEY UPDATE unreg_count = unreg_count+1");
-       		break;
-    	case 'robot':
-			$wpdb->query("INSERT INTO ".$hc_table." (post_id, bot_count) VALUES (".$post_id.", 1) ON DUPLICATE KEY UPDATE bot_count = bot_count+1");
-        	break;
- 		default:
- 			break;
-		}
+function logCount($count, $post_id) {
+	global $wpdb, $hc_table;
+	
+	switch ($count) {
+	case 'reg':
+    	$wpdb->query("INSERT INTO ".$hc_table." (post_id, reg_count) VALUES (".$post_id.", 1) ON DUPLICATE KEY UPDATE reg_count = reg_count+1");
+    	break;
+		case 'unreg':
+		$wpdb->query("INSERT INTO ".$hc_table." (post_id, unreg_count) VALUES (".$post_id.", 1) ON DUPLICATE KEY UPDATE unreg_count = unreg_count+1");
+   		break;
+	case 'robot':
+		$wpdb->query("INSERT INTO ".$hc_table." (post_id, bot_count) VALUES (".$post_id.", 1) ON DUPLICATE KEY UPDATE bot_count = bot_count+1");
+    	break;
+		default:
+			break;
 	}
+}
 
 /**
  * Output the number of views using the template tag userViews()
@@ -178,16 +178,16 @@ else
  * @param $before - html to place before output - default provided
  * @param $after - html to place after output - default provided
  */
-	function userViews($before = '<span class="views">Views: ', $after = '</span>') {
-		global $wpdb, $hc_table;
+function userViews($before = '<span class="views">Views: ', $after = '</span>') {
+	global $wpdb, $hc_table;
 
-		$post_id = get_the_ID();
-		$count = $wpdb->get_row("SELECT * FROM ".$hc_table." WHERE post_id = ".$post_id);
-		$views = number_format($count->reg_count + $count->unreg_count);
-		$views = $before.$views.$after;
+	$post_id = get_the_ID();
+	$count = $wpdb->get_row("SELECT * FROM ".$hc_table." WHERE post_id = ".$post_id);
+	$views = number_format($count->reg_count + $count->unreg_count);
+	$views = $before.$views.$after;
 
-		echo $views;
-	}
+	echo $views;
+}
 
 /**
  * Fetches the view count per post
@@ -195,129 +195,129 @@ else
  * @param $post_id - ID if the currently viewed post.
  * @return - array(bot_count, reg_count, unreg_count, human_count)
  */   
-	function get_views($post_id) {
-		global $wpdb, $hc_table, $post;
+function get_views($post_id) {
+	global $wpdb, $hc_table, $post;
 
-		$count = $wpdb->get_row("SELECT * FROM ".$hc_table." WHERE post_id = ".$post_id);
-		$viewcount[] = number_format($count->bot_count);
-		$viewcount[] = number_format($count->reg_count);
-		$viewcount[] = number_format($count->unreg_count);
-		$viewcount[] = number_format($count->reg_count + $count->unreg_count);
+	$count = $wpdb->get_row("SELECT * FROM ".$hc_table." WHERE post_id = ".$post_id);
+	$viewcount[] = number_format($count->bot_count);
+	$viewcount[] = number_format($count->reg_count);
+	$viewcount[] = number_format($count->unreg_count);
+	$viewcount[] = number_format($count->reg_count + $count->unreg_count);
 
-		return $viewcount;
-	}
+	return $viewcount;
+}
 
 /**
  * Display the view data on the post edit page.
  */   
-	function userViewsPostAdmin() {
-		add_meta_box(
-			'userviews', // id of the <div> we'll add
-			'Views', //title
-			'userViewsPostAdminData', // callback function that will echo the box content
-			'post', // where to add the box: on "post", "page", or "link" page
-			'normal', // The part of the page where the edit screen section should be shown ('normal' or 'advanced')
-			'low' // The priority within the context where the boxes should show ('high' or 'low')
-		);
-	}
+function userViewsPostAdmin() {
+	add_meta_box(
+		'userviews', // id of the <div> we'll add
+		'Views', //title
+		'userViewsPostAdminData', // callback function that will echo the box content
+		'post', // where to add the box: on "post", "page", or "link" page
+		'normal', // The part of the page where the edit screen section should be shown ('normal' or 'advanced')
+		'low' // The priority within the context where the boxes should show ('high' or 'low')
+	);
+}
 
-	if (is_admin()) 
-		add_action('admin_menu', 'userViewsPostAdmin');
+if (is_admin()) 
+	add_action('admin_menu', 'userViewsPostAdmin');
 	
 /**
  * Fill the data viewer with the data
  */   	
-	function userViewsPostAdminData() {
-		global $post;
-	       	 
-	        $views = get_views($post->ID);
-	
-			// total humans
-			echo '<p class="iconic humans"><strong>'.$views[3].'</strong> total humans</p>';
-			// registered humans
-			echo '<p class="iconic registered"><strong>'.$views[1].'</strong> registered humans</p>';
-			// unregistered humans
-			echo '<p class="iconic unregistered"><strong>'.$views[2].'</strong> unregistered humans</p>';
-			// robots
-			echo '<p class="iconic robots"><strong>'.$views[0].'</strong> robots</p>';
-	}
+function userViewsPostAdminData() {
+	global $post;
+       	 
+        $views = get_views($post->ID);
+
+		// total humans
+		echo '<p class="iconic humans"><strong>'.$views[3].'</strong> total humans</p>';
+		// registered humans
+		echo '<p class="iconic registered"><strong>'.$views[1].'</strong> registered humans</p>';
+		// unregistered humans
+		echo '<p class="iconic unregistered"><strong>'.$views[2].'</strong> unregistered humans</p>';
+		// robots
+		echo '<p class="iconic robots"><strong>'.$views[0].'</strong> robots</p>';
+}
 
 /**
  * Add a column to the edit post page
  */
-	function views_columns($defaults) {
-	    $defaults['views'] = __('Views');
-	    return $defaults;
-	}
+function views_columns($defaults) {
+    $defaults['views'] = __('Views');
+    return $defaults;
+}
 
 /**
  * fill that column with view data
  */
-	function views_custom_column($column_name, $id) {
-		global $hc_options;
-	    
-	    if( $column_name == 'views' ) {
+function views_custom_column($column_name, $id) {
+	global $hc_options;
+    
+    if( $column_name == 'views' ) {
 
-	        $views = get_views($id);
+        $views = get_views($id);
 
-			if ( $hc_options['display'] ) {
-				// total humans
-				echo '<span class="iconic humans">'.$views[3].'</span> ';
-				
-				if ( !$hc_options['displaydetail'] ) echo '<br />';
-				
-				// robots
-				echo '<span class="iconic robots">'.$views[0].'</span>';
-			}
+		if ( $hc_options['display'] ) {
+			// total humans
+			echo '<span class="iconic humans">'.$views[3].'</span> ';
 			
-			if ( $hc_options['display'] && $hc_options['displaydetail'] ) echo '<br />';
+			if ( !$hc_options['displaydetail'] ) echo '<br />';
 			
-			if ( $hc_options['displaydetail'] ) {
-				// registered humans
-				echo '<span class="iconic registered">'.$views[1].'</span> ';
-				
-				if ( !$hc_options['display'] ) echo '<br />';
-				
-				// unregistered humans
-				echo '<span class="iconic unregistered">'.$views[2].'</span>';
-			}
-	    }
-	}
+			// robots
+			echo '<span class="iconic robots">'.$views[0].'</span>';
+		}
+		
+		if ( $hc_options['display'] && $hc_options['displaydetail'] ) echo '<br />';
+		
+		if ( $hc_options['displaydetail'] ) {
+			// registered humans
+			echo '<span class="iconic registered">'.$views[1].'</span> ';
+			
+			if ( !$hc_options['display'] ) echo '<br />';
+			
+			// unregistered humans
+			echo '<span class="iconic unregistered">'.$views[2].'</span>';
+		}
+    }
+}
 
-	// only display the column if the options are enabled.
-	if ( $hc_options['display'] || $hc_options['displaydetail'] ) {
+// only display the column if the options are enabled.
+if ( $hc_options['display'] || $hc_options['displaydetail'] ) {
 
-		add_filter('manage_posts_columns', 'views_columns');
-		add_action('manage_posts_custom_column', 'views_custom_column', 10, 2);
+	add_filter('manage_posts_columns', 'views_columns');
+	add_action('manage_posts_custom_column', 'views_custom_column', 10, 2);
 
-	}
+}
 	
 /**
  * make the column the correct width
  */
-	function views_custom_column_css() {
-		global $hc_options;
+function views_custom_column_css() {
+	global $hc_options;
 
-		if ( $hc_options['display'] && $hc_options['displaydetail'] )
-			$width = '12';
-		else
-			$width = '8';
+	if ( $hc_options['display'] && $hc_options['displaydetail'] )
+		$width = '12';
+	else
+		$width = '8';
 
-		$plugurl = WP_PLUGIN_URL.'/'.str_replace(basename( __FILE__),"",plugin_basename(__FILE__));
-	?>
-	<style type="text/css">/* hitcounter */
-	.fixed .column-views {width:<?php echo $width; ?>%;}
-	.iconic {background: url(<?php echo $plugurl; ?>/icons/iconic.png) no-repeat; padding-left: 14px;}
-	.iconic.robots {background-position:0 0;}
-	.iconic.humans {background-position:0 -150px;}
-	.iconic.unregistered {background-position:0 -100px;}
-	.iconic.registered {background-position:0 -50px;}
-	</style>
-	
-	<?php
-	}
+	$plugurl = WP_PLUGIN_URL.'/'.str_replace(basename( __FILE__),"",plugin_basename(__FILE__));
+?>
+<style type="text/css">/* hitcounter */
+.fixed .column-views {width:<?php echo $width; ?>%;}
+.iconic {background: url(<?php echo $plugurl; ?>/icons/iconic.png) no-repeat; padding-left: 14px;}
+.iconic.robots {background-position:0 0;}
+.iconic.humans {background-position:0 -150px;}
+.iconic.unregistered {background-position:0 -100px;}
+.iconic.registered {background-position:0 -50px;}
+</style>
 
-	add_action('admin_head', 'views_custom_column_css');
+<?php
+}
+
+add_action('admin_head', 'views_custom_column_css');
 
 /////////////////////////////////////
 
@@ -327,38 +327,38 @@ else
  * http://codex.wordpress.org/Function_Reference/register_setting
  * http://planetozh.com/blog/2009/05/handling-plugins-options-in-wordpress-28-with-register_setting/
  */
-	function hitcounter_options_menu() {
-	
-		add_options_page('Hitcounter', 'Hitcounter', 'manage_options', 'hitcounter', 'hitcounter_settings_page');
-	
-		//call register settings function
-		add_action( 'admin_init', 'register_hitcounter_settings' );
-	
-	}
-	
-	// create custom plugin settings menu
-	add_action('admin_menu', 'hitcounter_options_menu');
+function hitcounter_options_menu() {
+
+	add_options_page('Hitcounter', 'Hitcounter', 'manage_options', 'hitcounter', 'hitcounter_settings_page');
+
+	//call register settings function
+	add_action( 'admin_init', 'register_hitcounter_settings' );
+
+}
+
+// create custom plugin settings menu
+add_action('admin_menu', 'hitcounter_options_menu');
 
 /**
  * Register our settings
  */
-	function register_hitcounter_settings() {
+function register_hitcounter_settings() {
 
-		register_setting( 'hitcounter-settings-group', 'hitcounter_options' );
+	register_setting( 'hitcounter-settings-group', 'hitcounter_options' );
 
-	}
+}
 	
 /**
  * Create the Settings page
  */
-	function hitcounter_settings_page() {
-		global $hc_options;
-	?>
+function hitcounter_settings_page() {
+	global $hc_options;
+?>
 	<div class="wrap">
 	
 	<div id="icon-options-general" class="icon32"><br /></div>
 	<h2>Hitcounter Options</h2>
-
+	
 	<form method="post" action="options.php">
 	    <?php settings_fields( 'hitcounter-settings-group' ); ?>
 	    
@@ -375,7 +375,7 @@ else
 		        	<span class="description">Show human and robots view counts.</span>
 		        </td>
 	        </tr>
-
+	
 	        <tr valign="top">
 		        <th scope="row"><span class="iconic registered">100</span> <span class="iconic unregistered">100</span></th>
 		        <td>
@@ -383,7 +383,7 @@ else
 		        	<span class="description">Show registered and unregistered view counts.</span>
 		        </td>
 	        </tr>
-
+	
 	        <tr valign="top">
 		        <th scope="row" colspan="2"><h3>Ignore Users&hellip;</h3></th>
 	        </tr>
@@ -395,7 +395,7 @@ else
 		        	<span class="description">Check to ignore post author.</span>
 		        </td>
 	        </tr>
-
+	
 	        <tr valign="top">
 		        <th scope="row">Role</th>
 		        <td>
@@ -409,7 +409,7 @@ else
 		        	<span class="description">Logged in users with this or a higher capability will not be counted.</span>
 		        </td>
 	        </tr>
-
+	
 		</table>
 	    
 	    <p class="submit">
@@ -419,6 +419,6 @@ else
 	</form>
 	</div>
 
-<?php }
-
+<?php 
+}
 ?>
